1d86f387a4c30f0166fe809b4b231628dde7ccef
LawrenceChou19 <lawrencechou19@gmail.com>
2024-01-06 20:37:41 +0800

    2024-01-06

---
+++ Operators and calculations.R
@@ -0,0 +1,6 @@
+#our first calculations
+quarter_1_sales <- 35657.98
+quarter_2_sales <- 43818.55
+midyear_sales <- quarter_1_sales + quarter_2_sales
+yearend_sales <- midyear_sales * 2
+yearend_sales
---
+++ Programming fundamentals.R
@@ -0,0 +1,7 @@
+print("Coding in R")
+?print()
+#Here is an example of a variable
+first_variable <- "This is my variable"
+second_variable <- 12.5
+vec_1 <- c(13,48.5,71,101.5,2)
+vec_1
