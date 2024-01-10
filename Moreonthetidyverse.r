e41416d9f6750162957d6e6a7cb757dc887f5de4
LawrenceChou19 <lawrencechou19@gmail.com>
2024-01-11 00:51:11 +0800

    20240111

---
+++ Moreonthetidyverse.r
@@ -0,0 +1,5 @@
+#8core tidyverse packages 
+#ggplot2 use for visualization
+#tidyr is use a package used for data cleanning to make tidy data
+#readr The most common function from readr is read_csv. This will import a CSV file into R.
+#dplyr
