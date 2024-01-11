551c34d982c995acc44ad453b42621aef17d80ac
LawrenceChou19 <lawrencechou19@gmail.com>
2024-01-11 13:54:26 +0800

    2024-01-11

--- Moreonthetidyverse.r
+++ Moreonthetidyverse.r
@@ -2,4 +2,8 @@
 #ggplot2 use for visualization
 #tidyr is use a package used for data cleanning to make tidy data
 #readr The most common function from readr is read_csv. This will import a CSV file into R.
-#dplyr
+#dplyr Dplyr offers a consistent set of functions that help you complete some common data manipulation tasks.
+#tibble Tibble works with data frames
+#purrr Purrr works with functions and vectors helping make your code easier to write and more expressive
+#Stringr Stringr includes functions that make it easier to work with strings.
+#Forcats Forcats provides tools that solve common problems with factors 
