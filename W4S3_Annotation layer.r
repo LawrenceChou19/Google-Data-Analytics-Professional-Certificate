6609393ece58fb45f769b298fb1bfb2823df4c00
LawrenceChou19 <lawrencechou19@gmail.com>
2024-01-19 16:42:57 +0800

    2024-01-19 Annotation Layer

---
+++ W4S3_Annotation layer.r
@@ -0,0 +1,17 @@
+install.packages("ggplot2")
+install.packages('palmerpenguins')
+library(ggplot2)
+library(palmerpenguins)
+
+ggplot(data=penguins)+
+    geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,color=species))+
+    labs(title="Palmer Penguins : Body Mass Vs. Flipper Length",subtitle="Sample of Three Penguin Species",
+        caption="Data collected by Dr. Kristen Gorman")+
+    annotate("text", x = 220,y=3500,label="The Gentoos are the largest",color="purple",fontface="bold",size=4.5,angle=25)
+
+p <- ggplot(data=penguins)+
+    geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,color=species))+
+    labs(title="Palmer Penguins : Body Mass Vs. Flipper Length",subtitle="Sample of Three Penguin Species",
+        caption="Data collected by Dr. Kristen Gorman")
+
+p+annotate("text",x=220,y=3500,label="The Gentoos are the largest")
